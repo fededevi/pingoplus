1ebca486c8ec056b9c5285904e3de67b11f0288d
Federico Devigili <f.devigili@alpitronic.it>
2022-08-15 09:44:19 +0200

    Added determinant calculation

--- math/mat4.h
+++ math/mat4.h
@@ -127,6 +127,33 @@ public:
         *this = rotation(axis,phi) * *this;
     }
 
+
+    T determinant()
+    {
+        const auto & a = this->e;
+        const T & a00 = a[0],  a01 = a[1],  a02 = a[2],  a03 = a[3];
+        const T & a10 = a[4],  a11 = a[5],  a12 = a[6],  a13 = a[7];
+        const T & a20 = a[8],  a21 = a[9],  a22 = a[10], a23 = a[11];
+        const T & a30 = a[12], a31 = a[13], a32 = a[14], a33 = a[15];
+
+        T b00 = a00 * a11 - a01 * a10;
+        T b01 = a00 * a12 - a02 * a10;
+        T b02 = a00 * a13 - a03 * a10;
+        T b03 = a01 * a12 - a02 * a11;
+        T b04 = a01 * a13 - a03 * a11;
+        T b05 = a02 * a13 - a03 * a12;
+        T b06 = a20 * a31 - a21 * a30;
+        T b07 = a20 * a32 - a22 * a30;
+        T b08 = a20 * a33 - a23 * a30;
+        T b09 = a21 * a32 - a22 * a31;
+        T b10 = a21 * a33 - a23 * a31;
+        T b11 = a22 * a33 - a23 * a32;
+
+        // Calculate the determinant
+        return b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+    }
+
+
     Mat4 inverse() const {
         Mat4 out;
         const auto & m = this->e;
